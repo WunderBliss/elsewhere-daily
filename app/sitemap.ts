c4d4f19cd2883181d0a67a12db9a008a5992fb49
Owen Orsetti <owen.orsetti@gmail.com>
2026-06-02 21:23:30 +0900

    feat: add sitemap and robots.txt

---
+++ app/sitemap.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from 'next'
+import { getPublishedArticleSlugs } from '@/lib/db/queries'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? ''
+  const slugs = await getPublishedArticleSlugs()
+
+  return [
+    { url: siteUrl, lastModified: new Date() },
+    ...slugs.map((s) => ({
+      url: `${siteUrl}/${s.slug}`,
+      lastModified: new Date(),
+    })),
+  ]
+}
